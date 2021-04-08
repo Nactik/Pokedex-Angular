4bc6de5b8695e7c9f02d05cfa5c6f0a4b6ad7d7f
Nathan Capiaux <nathan.capiaux@ig2i.centralelille.fr>
2021-04-08 20:40:43 +0200

    add MaterialsModules

--- src/app/pokemons/pokemons.module.ts
+++ src/app/pokemons/pokemons.module.ts
@@ -3,14 +3,34 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http'
 import { PokemonsRoutingModule } from './pokemons-routing.module';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { MatListModule } from "@angular/material/list";
+import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button'
+
+
 
 
 @NgModule({
-  declarations: [PokemonListComponent],
+  declarations: [PokemonListComponent, PokemonDetailComponent],
+  exports:[
+    PokemonListComponent,
+    PokemonDetailComponent
+  ],
   imports: [
     CommonModule,
     PokemonsRoutingModule,
     HttpClientModule,
+    MatListModule,
+    MatCardModule,
+    MatGridListModule,
+    MatChipsModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
+
 export class PokemonsModule { }
