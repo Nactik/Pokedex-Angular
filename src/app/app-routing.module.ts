647eaa49a07bd3fd912f66dd57a3a9f58c7a64df
Nathan Capiaux <nathan.capiaux@ig2i.centralelille.fr>
2021-04-08 20:39:33 +0200

    add root and detail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonDetailComponent } from './pokemons/components/pokemon-detail/pokemon-detail.component';
+import { PokemonListComponent } from './pokemons/components/pokemon-list/pokemon-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "", component: PokemonListComponent, pathMatch: 'full'},
+  {path : "detail/:id", component : PokemonDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
