b44bab399c902594daf735e214dd5ac59e563d39
Nathan Capiaux <nathan.capiaux@ig2i.centralelille.fr>
2021-04-09 15:12:17 +0200

    yes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PokedexComponent } from './pokemons/components/pokedex/pokedex.component';
-
 const routes: Routes = [
   {path : "", component: PokedexComponent, pathMatch: 'full'}
 ];
